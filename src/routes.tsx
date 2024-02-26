c61d34486f1710f8e38fa1d1685adafe754ca556
Sono Dennis Willbrand <sono00@web.de>
2024-02-26 13:39:47 +0100

    wip: adjust routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./pages";
 import Login from "./pages/Login";
 import { action as loginAction } from "./pages/Login/action";
 import Dashboard from "./pages/Dashboard";
+import Stocks from "./pages/Stocks";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
   {
     path: "/login",
     element: <Login />,
     action: loginAction,
   },
   {
-    path: "/dashboard",
+    path: "/",
     element: <Dashboard />,
+  },
+  {
+    path: "/stocks",
+    element: <Stocks />,
   },
 ]);
