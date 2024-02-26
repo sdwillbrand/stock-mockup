1de12d1fa443f2376c8daed83a4fcfc4f6a66f7b
Sono Dennis Willbrand <sono00@web.de>
2024-02-26 13:40:03 +0100

    wip: adjust links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="fixed top-0 w-full flex justify-end gap-2 p-2">
-      <Link to="/">Home</Link>
+      <Link to="/">Dashboard</Link>
       <Link to="/login">Login</Link>
+      <Link to="/stocks">Stocks</Link>
     </header>
   );
 };
